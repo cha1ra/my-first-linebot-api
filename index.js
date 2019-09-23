367ff9eb6a37ee8004c6cddb48931bf849e2180a
cha1ra <cha1ra.garnet@gmail.com>
2019-09-24 08:28:00 +0900

    Improve

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
       }else if (event.message.text.includes('お腹空いた')){
         // ぐるナビURL設定
         const url = `https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=${process.env.GURUNAVI_ID}&name=cafe`
+        console.log(url)
         const res = await fetch(url)
         resultText = res
       }else{
