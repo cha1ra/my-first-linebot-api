3865eef2a1546c0f30f84d15def37c35216eb74f
cha1ra <cha1ra.garnet@gmail.com>
2019-09-24 14:32:59 +0900

    Improve

--- index.js
+++ index.js
@@ -1,64 +1,63 @@
 // -----------------------------------------------------------------------------
 // モジュールのインポート
-const server = require("express")();
-const line = require("@line/bot-sdk");
-const fetch = require("node-fetch")
+const server = require('express')()
+const line = require('@line/bot-sdk')
+const fetch = require('node-fetch')
 
 // -----------------------------------------------------------------------------
 // パラメータ設定
 const line_config = {
   channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
   channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
-};
+}
 
 // -----------------------------------------------------------------------------
 // Webサーバー設定
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 3000)
 
 // APIコールのためのクライアントインスタンスを作成
-const bot = new line.Client(line_config);
-
+const bot = new line.Client(line_config)
 
 // -----------------------------------------------------------------------------
 // ルーター設定
 server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
   // 先行してLINE側にステータスコード200でレスポンスする。
-  res.sendStatus(200);
+  res.sendStatus(200)
 
   // すべてのイベント処理のプロミスを格納する配列。
-  let events_processed = [];
+  const events_processed = []
 
   // イベントオブジェクトを順次処理。
+
   req.body.events.forEach((event) => {
     // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
     console.log(event.type)
-    if (event.type == "message" && event.message.type == "text"){
+    if (event.type === 'message' && event.message.type === 'text') {
       let resultText = ''
 
-      if (event.message.text == 'こんにちは'){
+      if (event.message.text === 'こんにちは') {
         resultText = 'こんにちは'
-      }else if (event.message.text.includes('お腹空いた')){
+      } else if (event.message.text.includes('お腹空いた')) {
         // ぐるナビURL設定
         const url = `https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=${process.env.GURUNAVI_ID}&name=cafe`
         console.log(url)
-        fetch(url,{method: 'GET'})
+        fetch(url, { method: 'GET' })
           .then(res => res.json())
           .then(response => {
             console.log('Success:', JSON.stringify(response))
           })
-          .catch(error => console.error('Error:', error));
-      }else{
+          .catch(error => console.error('Error:', error))
+      } else {
         resultText = ''
       }
 
       if (resultText !== '') {
         // replyMessage()で返信し、そのプロミスをevents_processedに追加。
         events_processed.push(bot.replyMessage(event.replyToken, {
-          type: "text",
+          type: 'text',
           text: resultText
-        }));
+        }))
       }
-
 
       // // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
       // if (event.message.text == "こんにちは"){
@@ -69,12 +68,12 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
       //   }));
       // }
     }
-  });
+  })
 
   // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
   Promise.all(events_processed).then(
-      (response) => {
-        console.log(`${response.length} event(s) processed.`);
-      }
-  );
-});
+    (response) => {
+      console.log(`${response.length} event(s) processed.`)
+    }
+  )
+})
